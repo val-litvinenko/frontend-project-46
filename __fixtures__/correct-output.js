b354d3de6ddf165c433877f793e9b958f795fe0a
valerie_l <valeriya.litvinenko.work@gmail.com>
2023-11-05 15:36:33 +0700

    update fixtures

--- __fixtures__/correct-output.js
+++ __fixtures__/correct-output.js
@@ -1,154 +1,158 @@
 const defaultFile = `{
-  - follow: false,
-    host: hexlet.io,
-  - proxy: 123.234.53.22,
-  + timeout: 20,
-  - timeout: 50,
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
   + verbose: true
 }`;
 
 const sameFile = `{
-    follow: true,
-    host: hexlet.io,
+    follow: true
+    host: hexlet.io
     timeout: 100
 }`;
 
 const sameNestedFile = `{
-  common: {
-    setting1: Value 1,
-    setting2: 200,
-    setting3: true,
-    setting6: {
-      key: value,
-      doge: {
-        wow:
-      }
+    common: {
+        setting1: Value 1
+        setting2: 200
+        setting3: true
+        setting6: {
+            doge: {
+                wow: 
+            }
+            key: value
+        }
     }
-  },
-  group1: {
-    baz: bas,
-    foo: bar,
-    nest: {
-      key: value
+    group1: {
+        baz: bas
+        foo: bar
+        nest: {
+            key: value
+        }
     }
-  },
-  group2: {
-    abc: 12345,
-    deep: {
-      id: 45
+    group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
     }
-  }
 }`;
 
 const dif1File = `{
-  common: {
-    - setting1: Value 1
-    - setting2: 200
-      setting3: true
-      setting6: {
-          doge: {
-              wow: 
-          }
-      }
-  }
-  group1: {
-    - baz: bas
-      foo: bar
-      nest: {
-          key: value
-      }
-  }
+    common: {
+      - setting1: Value 1
+      - setting2: 200
+        setting3: true
+        setting6: {
+            doge: {
+                wow: 
+            }
+          - key: value
+        }
+    }
+    group1: {
+      - baz: bas
+        foo: bar
+        nest: {
+            key: value
+        }
+    }
 }`;
 
 const dif2File = `{
-  common: {
-    + setting1: Value 1
-    + setting2: 200
-      setting3: true
-      setting6: {
-          doge: {
-              wow: 
-          }
-      }
-  }
-  group1: {
-    + baz: bas
-      foo: bar
-      nest: {
-          key: value
-      }
-  }
+    common: {
+      + setting1: Value 1
+      + setting2: 200
+        setting3: true
+        setting6: {
+            doge: {
+                wow: 
+            }
+          + key: value
+        }
+    }
+    group1: {
+      + baz: bas
+        foo: bar
+        nest: {
+            key: value
+        }
+    }
 }`;
 
 const emptyFile = `{
+
 }`;
 
 const difValue = `{
-  common: {
-    + setting1: Value 5
-    - setting1: Value 1
-      setting2: 200
-    + setting3: false
-    - setting3: true
-      setting6: {
-          doge: {
-           + wow: !
-           - wow: 
-          }
-      }
-  }
-  group1: {
-      baz: bas
-      foo: bar
-      nest: {
-          key: value
-      }
-  }
+    common: {
+      - setting1: Value 1
+      + setting1: Value 5
+        setting2: 200
+      - setting3: true
+      + setting3: false
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: !
+            }
+            key: value
+        }
+    }
+    group1: {
+        baz: bas
+        foo: bar
+        nest: {
+            key: value
+        }
+    }
 }`;
 
 const nestedStructure = `{
-  common: {
-    + follow: false
-      setting1: Value 1
-    - setting2: 200
-    - setting3: true
-    + setting3: null
-    + setting4: blah blah
-    + setting5: {
-          key5: value5
-      }
-      setting6: {
-          doge: {
-            - wow: 
-            + wow: so much
-          }
-          key: value
-        + ops: vops
-      }
-  }
-  group1: {
-    - baz: bas
-    + baz: bars
-      foo: bar
-    - nest: {
-          key: value
-      }
-    + nest: str
-  }
-- group2: {
-      abc: 12345
-      deep: {
-          id: 45
-      }
-  }
-+ group3: {
-      deep: {
-          id: {
-              number: 45
-          }
-      }
-      fee: 100500
-  }
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
 }`;
 
 export {
