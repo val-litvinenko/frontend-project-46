267e342203c5e21727a2fbc54c986ccaf3ff8e54
valerie_l <valeriya.litvinenko.work@gmail.com>
2023-12-04 17:43:36 +0700

    Add new logic

--- generate-diff.js
+++ generate-diff.js
@@ -1,37 +1,27 @@
 import _ from 'lodash';
 
-const generateDiff = (obj1, obj2, parent = null, depth = 1) => {
-  const depthFlag = {};
+const generateDiff = (obj1, obj2) => {
   const keys1 = Object.keys(obj1);
   const keys2 = Object.keys(obj2);
   const keys = _.union(keys1, keys2);
-  const diff = keys.flatMap((key) => {
+  const diff = keys.map((key) => {
     const key1 = Object.hasOwn(obj1, key) ? key : null;
     const key2 = Object.hasOwn(obj2, key) ? key : null;
     const isObjectV1 = _.isObject(obj1[key]);
     const isObjectV2 = _.isObject(obj2[key]);
-    const value1 = isObjectV1 && isObjectV2 ? depthFlag : obj1[key];
-    const value2 = isObjectV1 && isObjectV2 ? depthFlag : obj2[key];
     let children = [];
-    if (value1 === depthFlag && value2 === depthFlag) {
-      children = generateDiff(
-        (isObjectV1 ? obj1[key] : {}),
-        (isObjectV2 ? obj2[key] : {}),
-        key,
-        depth + 1,
-      );
+    if (isObjectV1 && isObjectV2) {
+      children = generateDiff(obj1[key], obj2[key]);
     }
-    return [
-      {
-        key1,
-        key2,
-        value1,
-        value2,
-        parent,
-        depth,
-      },
-      ...children,
-    ];
+    const value1 = isObjectV1 && isObjectV2 ? null : obj1[key];
+    const value2 = isObjectV1 && isObjectV2 ? null : obj2[key];
+    return {
+      key1,
+      key2,
+      value1,
+      value2,
+      children,
+    };
   });
   return diff;
 };
--- make-diff.js
+++ make-diff.js
@@ -1,83 +1,9 @@
 import path from 'node:path';
 import { cwd } from 'node:process';
-import _ from 'lodash';
 import { readFileSync } from 'node:fs';
 import parser from './parsers.js';
 import generateDiff from './generate-diff.js';
-
-const generateString = (depth, symbol, key, value) => `${depth}${symbol} ${key}: ${value}`;
-const symbols = [' ', '+', '-'];
-const COUNT_INDENT = 4;
-
-const buildGap = (depth) => {
-  const gapLevel = depth * COUNT_INDENT - 2;
-  return ' '.repeat(gapLevel);
-};
-
-const buildObjectString = (object, depth) => {
-  const keys = Object.keys(object);
-  const objectLines = keys.map((key) => {
-    if (_.isObject(object[key])) {
-      return generateString(
-        buildGap(depth + 1),
-        symbols[0],
-        key,
-        buildObjectString(object[key], depth + 1),
-      );
-    }
-    return generateString(buildGap(depth + 1), symbols[0], key, object[key]);
-  });
-  return `{
-${objectLines.join('\n')}
-${' '.repeat((depth) * COUNT_INDENT)}}`;
-};
-
-const stylish = (diffArr, originArr, depth = 1) => {
-  const arrayWithCurrentDepth = diffArr.filter((object) => object.depth === depth);
-  const outputArray = _.sortBy(arrayWithCurrentDepth, (o) => o.key1 ?? o.key2).map((obj) => {
-    const gap = buildGap(obj.depth);
-    if (obj.key1 === obj.key2 && obj.value1 === obj.value2) {
-      if (_.isObject(obj.value1)) {
-        const string = stylish(
-          originArr.filter((child) => child.parent === obj.key1),
-          originArr,
-          depth + 1,
-        );
-        return generateString(gap, symbols[0], obj.key1, string);
-      }
-      return generateString(gap, symbols[0], obj.key1, obj.value1);
-    }
-    if (obj.key1 === obj.key2 && obj.value1 !== obj.value2) {
-      if (_.isObject(obj.value1)) {
-        return `${generateString(gap, symbols[2], obj.key1, buildObjectString(obj.value1, obj.depth))}
-${generateString(gap, symbols[1], obj.key2, obj.value2)}`;
-      }
-      if (_.isObject(obj.value2)) {
-        return `${generateString(gap, symbols[2], obj.key1, obj.value1)}
-${generateString(gap, symbols[1], obj.key2, buildObjectString(obj.value2, obj.depth))}`;
-      }
-      return `${generateString(gap, symbols[2], obj.key1, obj.value1)}
-${generateString(gap, symbols[1], obj.key2, obj.value2)}`;
-    }
-    if (obj.key2 === null) {
-      if (_.isObject(obj.value1)) {
-        return generateString(gap, symbols[2], obj.key1, buildObjectString(obj.value1, obj.depth));
-      }
-      return generateString(gap, symbols[2], obj.key1, obj.value1);
-    }
-    if (obj.key1 === null) {
-      if (_.isObject(obj.value2)) {
-        return generateString(gap, symbols[1], obj.key2, buildObjectString(obj.value2, obj.depth));
-      }
-      return generateString(gap, symbols[1], obj.key2, obj.value2);
-    }
-    return '';
-  });
-  const output = outputArray.join('\n');
-  return `{
-${output}
-${' '.repeat((depth - 1) * COUNT_INDENT)}}`;
-};
+import formatter from './formatters/index.js';
 
 const genDiff = (filepath1, filepath2, options) => {
   const firstPathFile = path.resolve(cwd(), filepath1);
@@ -87,9 +13,7 @@ const genDiff = (filepath1, filepath2, options) => {
   const parsedFirstFile = parser(firstPathFile, firstString);
   const parsedSecondFile = parser(secondPathFile, secondString);
   const difference = generateDiff(parsedFirstFile, parsedSecondFile);
-  if (options.format === 'stylish') {
-    console.log(stylish(difference, difference));
-  }
+  console.log(formatter(options.format, difference));
 };
 
 export default genDiff;
