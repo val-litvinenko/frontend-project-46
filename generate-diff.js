1a56a224cef483c93b669f20d91bfdca8d88eb5c
valerie_l <valeriya.litvinenko.work@gmail.com>
2024-04-11 20:37:13 +0200

    fix code climate issues

--- generate-diff.js
+++ generate-diff.js
@@ -1,26 +1,32 @@
 import _ from 'lodash';
 
+const hasValue = (obj1, obj2, key) => _.isObject(obj1[key]) && _.isObject(obj2[key]);
+
+const generateDiffItem = (obj1, obj2, key) => {
+  const key1 = Object.hasOwn(obj1, key) ? key : null;
+  const key2 = Object.hasOwn(obj2, key) ? key : null;
+
+  const value1 = hasValue(obj1, obj2, key) ? null : obj1[key];
+  const value2 = hasValue(obj1, obj2, key) ? null : obj2[key];
+  return {
+    key1,
+    key2,
+    value1,
+    value2,
+  };
+};
+
 const generateDiff = (obj1, obj2) => {
   const keys1 = Object.keys(obj1);
   const keys2 = Object.keys(obj2);
   const keys = _.union(keys1, keys2);
   const diff = keys.map((key) => {
-    const key1 = Object.hasOwn(obj1, key) ? key : null;
-    const key2 = Object.hasOwn(obj2, key) ? key : null;
     let children = [];
-    const hasValue = _.isObject(obj1[key]) && _.isObject(obj2[key]);
-    if (hasValue) {
+    if (hasValue(obj1, obj2, key)) {
       children = generateDiff(obj1[key], obj2[key]);
     }
-    const value1 = hasValue ? null : obj1[key];
-    const value2 = hasValue ? null : obj2[key];
-    return {
-      key1,
-      key2,
-      value1,
-      value2,
-      children,
-    };
+    const diffItem = generateDiffItem(obj1, obj2, key);
+    return { ...diffItem, children };
   });
   return diff;
 };
